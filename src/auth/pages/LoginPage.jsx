89df829556b5e1c7bb0f36ccada384fc908ba029
d3us <dnarino@gmail.com>
2023-09-06 16:38:25 -0500

    create html structure for LoginPage RegisterPage and define AuthLayout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,60 @@
-export const LoginPage= () => {
+import { Link as RouterLink } from 'react-router-dom';
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from '../layout/AuthLayout';
+
+export const LoginPage = () => {
   return (
-      <>
-        <h1>Login Page</h1>
-      </>
+    <>
+      <AuthLayout title={'Login'}>
+      <form>
+            <Grid
+              container
+            >
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <TextField
+                  label={'email'}
+                  type={'email'}
+                  placeholder={'miemail@mail.com'}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sx={{ mt: 2 }}>
+                <TextField
+                  label={'password'}
+                  type={'password'}
+                  fullWidth
+                />
+              </Grid>
+              <Grid container spacing={2} sx={{ m: 2 }}>
+                <Grid item xs={12} sm={6}>
+                  <Button variant="contained" fullWidth>
+                    Login
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Button variant="contained" fullWidth>
+                    <Google />
+                    <Typography sx={{ ml: 1 }}>Google</Typography>
+                  </Button>
+                </Grid>
+              </Grid>
+              <Grid container direction={'row'} justifyContent={'end'} >
+                <Link
+                  color={'inherit'}
+                  component={
+                    RouterLink
+                  }
+                  to={'/auth/register'}
+                >
+                  Create Account
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+      </AuthLayout>
+         
+        
+    </>
   );
 };
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,64 @@
+import { Link as RouterLink } from 'react-router-dom';
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+import { Edit, Google, WineBarTwoTone } from "@mui/icons-material";
+
 export const RegisterPage = () => {
   return (
-      <>
-        <h1>RegisterPage</h1>
-      </>
+    <>
+      <AuthLayout title={'Register'}>
+        <form>
+          <Grid
+            container
+          >
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label={'Your Name'}
+                type={'text'}
+                placeholder={'Jhon Doe'}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label={'email'}
+                type={'email'}
+                placeholder={'miemail@mail.com'}
+                fullWidth
+              />
+            </Grid>
+            
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField
+                label={'password'}
+                type={'password'}
+                fullWidth
+              />
+            </Grid>
+
+            <Grid container spacing={2} sx={{ m: 2 }}>
+              <Grid item xs={12} >
+                <Button variant="contained" fullWidth>
+                  <Edit />
+                  <Typography sx={{ ml: 1 }}>Register</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction={'row'} justifyContent={'end'} >
+              <Typography sx={{mr:1}} >Are you Register?</Typography>
+                <Link
+                  color={'inherit'}
+                  component={
+                    RouterLink
+                  }
+                  to={'/auth/login'}
+                >
+                  Login
+                </Link>
+              </Grid>
+          </Grid>
+        </form>
+      </AuthLayout>
+    </>
   );
 };
