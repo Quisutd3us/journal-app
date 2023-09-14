120124193ddf13978999b7fff60af3ea82fb84de
d3us <dnarino@gmail.com>
2023-09-13 20:20:35 -0500

    creating initial reducers for journalSlice.js

--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -4,14 +4,42 @@ export const journalSlice = createSlice({
     name: 'journal',
     initialState: {
         isSaving: true,
+        messageSaved: '',
+        notes: [],
+        activeNote: null,
+        // activeNote:{
+        // id:'ABC123',
+        // title:'',
+        // body:'',
+        // date:123456,
+        // imageURLs :[] // [https://image1.jpg, https://image1.jpg, ,https://image1.jpg,]
+        // }
     },
     reducers: {
-        increment: (state, /* action */ ) => {
-            state.counter += 1;
+        addNewEmptyNote: (state, action) => {
+
         },
+        setActiveNote: (state, action) => {
+
+        },
+        setSaving: (state, action) => {
+
+        },
+        updateNote: (state, action) => {
+
+        },
+        delNoteByID: (state, action) => {
+
+        }
     }
 });
 
 
 // Action creators are generated for each case reducer function
-export const { increment } = journalSlice.actions;
+export const {
+    addNewEmptyNote,
+    setActiveNote,
+    setSaving,
+    updateNote,
+    delNoteByID
+} = journalSlice.actions;
